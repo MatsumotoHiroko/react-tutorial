4670378ba57cb3ff45dd02cfb56bdfdd77f528b7
matsumoto <hm.hiroko.matsumoto@gmail.com>
2019-06-03 22:17:20 +0900

    Updates

---
+++ precache-manifest.8f35d449cb7aa46da8ff2dbfe728845f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8289beaa409ba38c0f55911bb4a48c1c",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "6068177129ed5a1ec084",
+    "url": "/react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "6068177129ed5a1ec084",
+    "url": "/react-tutorial/static/js/main.a430d05f.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
